fd84571d46eb9c371fc7af86d7f952cab865c140
JStivenJ <jsjimenezu@unal.edu.co>
2024-07-05 08:40:07 -0500

    Update processing.py
    nperseg and noverlap

--- gcpds/em_spectrum_monitor/processing.py
+++ gcpds/em_spectrum_monitor/processing.py
@@ -81,8 +81,23 @@ class Processing:
 
         signal = signal - np.mean(signal)
 
-        # YN: Revisar valores de nperseg y noverlap
-        f, Pxx = welch(signal, fs=fs, nperseg=1024, window='hann', noverlap=512)
+        """
+        nperseg: int, optional
+            Specifies the number of points in each segment. During the calculation of the 
+            PSD using the Welch method, the signal is divided into segments, 
+            and the PSD is calculated for each segment
+
+            Length of each segment. Defaults to None, but if window is str or tuple, 
+            is set to 256, and if window is array_like, is set to the length of the window.
+
+        noverlap: int, optional
+            Specifies the number of overlap points between consecutive segments. 
+            By overlaying the segments, a smoother and less noisy estimate of the PSD is obtained
+
+            Number of points to overlap between segments. If None, noverlap = nperseg // 2. Defaults to None.
+        """
+
+        f, Pxx = welch(signal, fs=fs, nperseg=1024, window='hann')
 
         f = np.fft.fftshift(f)
         Pxx = np.fft.fftshift(Pxx)
