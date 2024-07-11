7a8a2d4538eaeb5a2b1ade876c372c6866af1212
DANN9907 <dloaiza180@gmail.com>
2024-07-11 17:40:38 -0500

    Comentarios y comprobación de samples
    Se agregan los comentarios de cada método y la comprobación de que las muestras siguen siendo complejas

--- gcpds/em_spectrum_monitor/test_monitor.py
+++ gcpds/em_spectrum_monitor/test_monitor.py
@@ -4,20 +4,30 @@ import numpy as np
 from monitor import Scanning
 
 class TestScanning(unittest.TestCase):
+    """"""
 
     @classmethod
     def setUpClass(cls):
+        """
+        Initialize the Scanning object with the given parameters for unit testing.
+        """
         cls.scanning = Scanning(vga_gain=0, lna_gain=0, sample_rate=20e6, overlap=0, time_to_read=0.01)
         cls.wide_samples = cls.scanning.scan(88e6, 108e6)
         cls.samples = cls.scanning.concatenate(cls.wide_samples, 'mean')
 
     def test_initialization(self):
+        """
+        Check the correct setting of the parameters for performing the scan.
+        """
         self.assertEqual(self.scanning.hackrf.vga_gain, 0)
         self.assertEqual(self.scanning.hackrf.lna_gain, 0)
         self.assertEqual(self.scanning.hackrf.sample_rate, 20e6)
-        self.assertEqual(self.scanning.overlap, self.scanning.overlap)
 
     def test_scanning(self):
+        """
+        Verify that the structure of the read data is as expected, 
+        and that each element within the list is of the expected type
+        """
         self.assertIsInstance(self.wide_samples, list)
         self.assertGreater(len(self.wide_samples), 0)
         for stay in self.wide_samples:
@@ -35,10 +45,12 @@ class TestScanning(unittest.TestCase):
             self.assertIsInstance(stay['overlap'], int)
             
     def test_concat(self):
+        """
+        Check that the size of the concatenated samples is as expected and type of some samples.
+        """
         self.assertEqual(len(self.samples), self.scanning.samples_to_read*len(self.wide_samples)-(self.scanning.overlap_size*(len(self.wide_samples)-1)))
-
-        
-        
+        self.assertIsInstance(self.samples[0], complex)
+        self.assertIsInstance(self.samples[-1], complex)
 
 
 
