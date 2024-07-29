a06cff2280ef22bf65f611f0e0060e66f001c79b
DANN9907 <dloaiza180@gmail.com>
2024-07-28 20:35:03 -0500

    Create test_detection
    Se crea script para las pruebas unitarias de el script detection, se validan los formatos en los que se retornan las variables

---
+++ gcpds/em_spectrum_monitor/test_detection.py
@@ -0,0 +1,28 @@
+import unittest
+import numpy as np
+from processing import Processing
+from detection import Detection
+
+class TestScanning(unittest.TestCase):
+    """"""
+
+    @classmethod
+    def setUpClass(self):
+        """
+        Initial setup for the detection object
+        """
+        signal = np.load('database/Samples 88.0 and 108.0MHz with time to read 0.01s and 0MHz overlap.npy')
+        pros = Processing()
+        f, Pxx = pros.welch(signal, 20e6)
+
+        self.detec = Detection()
+        self.peak_freqs, self.peak_powers, self.detections = self.detec.power_based_detection(f, Pxx)
+
+    def test_detection(self):
+        self.assertIsInstance(self.peak_freqs, np.ndarray)
+        self.assertIsInstance(self.peak_powers, np.ndarray)
+        self.assertIsInstance(self.detections, list)
+    
+
+if __name__ == '__main__':
+    unittest.main()
